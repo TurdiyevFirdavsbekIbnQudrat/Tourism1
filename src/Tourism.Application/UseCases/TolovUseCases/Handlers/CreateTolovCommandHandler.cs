b9415d380457f22841d5124b823789033a97bcd3
TurdiyevFirdavsbekIbnQudrat <f1rdavsb3kturdiy3vibnqudrat@gmail.com>
2024-01-16 03:04:45 +0500

    CreateTolovCommandHandler qoshild

--- src/Tourism.Application/UseCases/TolovUseCases/Handlers/CreateTolovCommandHandler.cs
+++ src/Tourism.Application/UseCases/TolovUseCases/Handlers/CreateTolovCommandHandler.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Tourism.Application.Abstraction;
+using Tourism.Application.UseCases.TolovUseCases.Commands;
+using Toursim.Domain.Entities;
 
 namespace Tourism.Application.UseCases.TolovUseCases.Handlers
 {
-    internal class CreateTolovCommandHandler
+    public class CreateTolovCommandHandler : IRequestHandler<CreateTolovCommand, Tolov>
     {
+        private readonly ITourismDbContext _tourism;
+
+        public CreateTolovCommandHandler(ITourismDbContext tourism)
+        {
+            _tourism = tourism;
+        }
+
+        public async Task<Tolov> Handle(CreateTolovCommand request, CancellationToken cancellationToken)
+        {
+            var command = new Tolov()
+            {
+                foydalanuvchiId = request.foydalanuvchiId,
+                tolovQiymati = request.tolovQiymati
+            };
+            try
+            {
+                await _tourism.tolovlar.AddAsync(command);
+                _tourism.SaveChangesAsync(cancellationToken);
+                return command;
+            }
+            catch
+            {
+                return new Tolov();
+            }
+
+        }
     }
 }
--- src/Tourism.Application/UseCases/TolovUseCases/Handlers/GetByIdTolovCommandHandler.cs
+++ src/Tourism.Application/UseCases/TolovUseCases/Handlers/GetByIdTolovCommandHandler.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tourism.Application.UseCases.TolovUseCases.Commands;
+using Tourism.Application.UseCases.TolovUseCases.Queries;
+using Toursim.Domain.Entities;
 
 namespace Tourism.Application.UseCases.TolovUseCases.Handlers
 {
-    internal class GetByIdTolovCommandHandler
+    public class GetByIdTolovCommandHandler:IRequestHandler<GetByIdTolovCommand,Tolov>
     {
     }
 }
