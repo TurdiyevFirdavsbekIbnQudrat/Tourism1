66168d004582f4daecadd3994b2c830e23752d0d
TurdiyevFirdavsbekIbnQudrat <f1rdavsb3kturdiy3vibnqudrat@gmail.com>
2024-01-16 02:53:29 +0500

    TolovUseCases qoshildi

---
+++ src/Tourism.Application/UseCases/TolovUseCases/Queries/GetAllTolovCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Toursim.Domain.Entities;
+
+namespace Tourism.Application.UseCases.TolovUseCases.Queries
+{
+    public class GetAllTolovCommand : IRequest<IEnumerable<Tolov>>
+    {
+    }
+}
---
+++ src/Tourism.Application/UseCases/TolovUseCases/Queries/GetByIdTolovCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Toursim.Domain.Entities;
+
+namespace Tourism.Application.UseCases.TolovUseCases.Queries
+{
+    public class GetByIdTolovCommand : IRequest<Tolov>
+    {
+        public int id { get; set; }
+    }
+}
