5e1974831ba2e3e15a294449300b5626b13dfe58
TurdiyevFirdavsbekIbnQudrat <f1rdavsb3kturdiy3vibnqudrat@gmail.com>
2024-01-16 02:45:56 +0500

    GetByIdFikrCommandHandler qoshild

--- src/Tourism.Application/UseCases/FikrUseCases/Handlers/GetByIdFikrCommandHandler.cs
+++ src/Tourism.Application/UseCases/FikrUseCases/Handlers/GetByIdFikrCommandHandler.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tourism.Application.Abstraction;
+using Tourism.Application.UseCases.FikrUseCases.Queries;
+using Tourism.Application.UseCases.FoydalanuvchiUseCases.Queries;
+using Toursim.Domain.Entities;
 
 namespace Tourism.Application.UseCases.FikrUseCases.Handlers
 {
-    internal class GetByIdFikrCommandHandler
+    public class GetByIdFikrCommandHandler:IRequestHandler<GetByIdFikrCommand,Fikr>
     {
+        private readonly ITourismDbContext _tourism;
+
+        public GetByIdFikrCommandHandler(ITourismDbContext tourism)
+        {
+            _tourism = tourism;
+        }
+        public async Task<Fikr> Handle(GetByIdFikrCommand request, CancellationToken cancellationToken)
+        {
+            Fikr result = await _tourism.fikrlar.FirstOrDefaultAsync(x => x.id == request.id);
+            if (result != null)
+            {
+                return result;
+            }
+            return new Fikr();
+        }
     }
 }
