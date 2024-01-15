d6c85f973e6981305f8320352c54ef639ffeb2de
TurdiyevFirdavsbekIbnQudrat <f1rdavsb3kturdiy3vibnqudrat@gmail.com>
2024-01-16 03:51:20 +0500

    added ShaharController

---
+++ src/Tourism.API/Controllers/ShaharController.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Tourism.API.Dtos.ShaharDto;
+using Tourism.Application.UseCases.FoydalanuvchiUseCases.Queries;
+using Tourism.Application.UseCases.ShaharlarUseCases.Commands;
+using Tourism.Application.UseCases.ShaharlarUseCases.Queries;
+
+namespace Tourism.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ShaharController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ShaharController(IMediator _mediator)
+        {
+            mediator = _mediator;
+        }
+        //    [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateShaharlarAsync(CreateShaharlarCommand command)
+        {
+            var result = await mediator.Send(command);
+            return Ok(result);
+        }
+        //  [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllShaharlarAsync()
+        {
+            return Ok(await mediator.Send(new GetAllShaharlarCommand()));
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async ValueTask<IActionResult> GetShaharlarByIdAsync(int id)
+        {
+            GetByIdFoydalanuvchiCommand command = new GetByIdFoydalanuvchiCommand { id = id };
+            return Ok(await mediator.Send(command));
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteFoydalanuvchiById(int id)
+        {
+            DeleteShaharlarCommand command = new DeleteShaharlarCommand() { id = id };
+            return Ok(await mediator.Send(command));
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateFoydalanuvchiById(UpdateShaharDto request, int id)
+        {
+            UpdateShaharlarCommand command = new UpdateShaharlarCommand()
+            {
+                nomi = request.nomi,
+                rasm = request.rasm,
+                id = id,
+            };
+            return Ok(await mediator.Send(command));
+        }
+    }
+}
---
+++ src/Tourism.API/Dtos/ShaharDto/UpdateShaharDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Tourism.API.Dtos.ShaharDto
+{
+    public class UpdateShaharDto
+    {
+
+        public string nomi { get; set; }
+        public string rasm { get; set; }
+    }
+
+}
