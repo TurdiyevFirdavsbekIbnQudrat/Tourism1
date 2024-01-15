732024633ea049c5c359c5190ad28bd1968be231
TurdiyevFirdavsbekIbnQudrat <f1rdavsb3kturdiy3vibnqudrat@gmail.com>
2024-01-16 03:12:35 +0500

    added GetByIdTolovCommandHandler

--- src/Tourism.Application/UseCases/TolovUseCases/Handlers/GetByIdTolovCommandHandler.cs
+++ src/Tourism.Application/UseCases/TolovUseCases/Handlers/GetByIdTolovCommandHandler.cs
@@ -1,16 +1,28 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tourism.Application.UseCases.TolovUseCases.Commands;
+using Microsoft.EntityFrameworkCore;
+using Tourism.Application.Abstraction;
+using Tourism.Application.UseCases.FoydalanuvchiUseCases.Queries;
 using Tourism.Application.UseCases.TolovUseCases.Queries;
 using Toursim.Domain.Entities;
 
 namespace Tourism.Application.UseCases.TolovUseCases.Handlers
 {
-    public class GetByIdTolovCommandHandler:IRequestHandler<GetByIdTolovCommand,Tolov>
+    public class GetByIdTolovCommandHandler : IRequestHandler<GetByIdTolovCommand, Tolov>
     {
+        private readonly ITourismDbContext _tourism;
+
+        public GetByIdTolovCommandHandler(ITourismDbContext tourism)
+        {
+            _tourism = tourism;
+        }
+        public async Task<Tolov> Handle(GetByIdTolovCommand request, CancellationToken cancellationToken)
+        {
+            Tolov result = await _tourism.tolovlar.FirstOrDefaultAsync(x => x.id == request.id);
+            if (result != null)
+            {
+                return result;
+            }
+            return new Tolov();
+        }
     }
 }
